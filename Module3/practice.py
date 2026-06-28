b3463fcef6b447840166ee00a524b4fc07885322
Ollie <a55990a00@gmail.com>
2026-06-28 17:35:07 +0800

    6/28上課內容(input,format,string,f-string,tuple,set)

--- Module3/practice.py
+++ Module3/practice.py
@@ -5,6 +5,25 @@
 # 數字太小 要提示「請輸入更大的數字」
 # 使用者猜對要回傳「恭喜中獎」
 
+# 強制結束 Ctrl + C
+
+answer = 60
+
+print(type(answer))
+while True:
+    user_input = int(input("請輸入1~100數字: "))
+    if user_input < 1 or user_input > 100:
+        print("超出範圍請重新輸入\n")
+    elif user_input > answer:
+        # 在第二行print() = 在字串內\n = 換行
+        print("請輸入更小的數字")
+        print()
+    elif user_input < answer:
+        print("請輸入更大的數字")
+        print()
+    elif user_input == 60:
+        print("恭喜中獎")
+        break
 
 
 
