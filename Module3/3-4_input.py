992cd4bd57db04c0fcfa44ca7a6aa794a35e2197
Ollie <a55990a00@gmail.com>
2026-07-12 09:54:06 +0800

    複習

--- Module3/3-4_input.py
+++ Module3/3-4_input.py
@@ -4,10 +4,13 @@ user_input = input("請輸入數字: ")
 # 取得使用者輸入的資料
 print(user_input)
 print(type(user_input))
-result = user_input +10
+# result = user_input +10，此時會報錯，因為其資料型別目前還是 string
 
 # 將使用者輸入強制轉型成 int
 user_input = int(user_input)
 print(type(user_input))
 result = user_input + 10
 print(result)
+
+# cd ./module3
+# python ./3-4_input.py 
--- Module3/practice_ChatGPT.py
+++ Module3/practice_ChatGPT.py
@@ -1,25 +1,38 @@
+# 終極密碼 讓使用者能夠重複猜數字，直到猜對為止
+# 告訴使用者需要輸入的數字範圍 input()
+# 超出範圍要顯示「超出範圍請重新輸入」
+# 數字太大 要提示「請輸入更小的數字」
+# 數字太小 要提示「請輸入更大的數字」
+# 使用者猜對要回傳「恭喜中獎」
+
+# 強制結束 Ctrl + C
+
+# 報錯時 提示使用者「請輸入數字 而不是文字」
 import random
 
 while True:
     # 每次開始新遊戲都重新產生答案
     answer = random.randint(1, 100)
+    print(answer)
     count = 0
-
-    # 一場遊戲
+    
     while True:
         try:
             user_input = int(input("請輸入 1~100 的數字："))
         except ValueError:
             print("請輸入數字，而不是文字！")
             continue
+            # 先讓`except`處理資料型別，正確才 continue
 
         if user_input < 1 or user_input > 100:
             print("超出範圍，請重新輸入！")
             continue
+            # 第一層處理資料範圍
 
         if user_input > answer:
             print("請輸入更小的數字")
-            count += 1
+            count += 1 # 錯誤計數器放在這裡
+            # 此時才正式進入遊戲，注意這裡還是 if
 
         elif user_input < answer:
             print("請輸入更大的數字")
@@ -32,7 +45,7 @@ while True:
         print(f"目前已猜錯 {count} 次")
 
         if count == 3:
-            print(f"你已經猜錯 3 次了！答案是 {answer}")
+            print(f"你已經猜錯 3 次了！答案是 {answer} ，嫩啦！")
             break
 
         print()
